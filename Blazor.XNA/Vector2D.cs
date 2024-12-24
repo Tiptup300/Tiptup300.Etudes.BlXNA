e64ab8fa22bb56121db651930c7fcc508bf088f4
Matthew Deweese <tiptup300@gmail.com>
2024-12-24 09:32:49 -0500

    init

---
+++ Blazor.XNA/Vector2D.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.XNA
+{
+    public record Vector2D(float x, float y)
+    {
+
+    }
+}
